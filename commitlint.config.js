6f312a05dd85e51203cc8a6116ef8830821e5a1c
Matthew Cain <mcain@ipipeline.com>
2020-03-06 13:52:41 -0500

    feat(NGSD-1111): Test to confirm parser breaking check

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,18 @@
+
+const jiraProjects = 'NGSD|CESBASE';
+
+const regex = new RegExp(`^(\w*)\((({jiraProjects})-\d*)\):\s(.*)$`);
+
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-	rules: {
-    'scope-case': [0, 'always', 'lower-case']
-  }
+    parserPreset: {
+        'parserOpts': {
+            'headerPattern': regex,
+            'headerCorrespondence': ['type', 'scope', 'ticket', 'subject']
+        }
+    },
+    rules: {
+        'scope-case': [2, 'always', 'upper-case'],
+        'subject-case': [2, 'always', 'sentence-case']
+    }
 }

e8f45bd0e287c4fed2957ebb9414f7afb7ef21bc
Matthew Cain <mcain@ipipeline.com>
2020-03-06 15:02:19 -0500

    feat(commit msg): Test
    NGSD-1111:

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,12 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    "rules": {
+        "references-empty": [2, "never"],
+        "subject-case": [2, "always", "sentence-case"],
+    },
+    "parserPreset": {
+        "parserOpts": {
+            "issuePrefixes": ["NGSD-", "CESBASE-"]
+        }
+    }
 };

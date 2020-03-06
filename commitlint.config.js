af59102c9d597b4f2e667920dfe39b131853ef44
Matthew Cain <mcain@ipipeline.com>
2020-03-06 14:24:12 -0500

    feat(NGSD-1111): Test commitlint in package.json NGSD-1111

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,3 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-        'scope-case': [2, 'always', 'upper-case'],
-        'subject-case': [2, 'always', 'sentence-case'],
-		'references-empty': [2, 'never']
-    },
-	parserPreset: {
-		parserOpts: {
-			issuePrefixes: ['TEST-']
-		}
-	}
 };
